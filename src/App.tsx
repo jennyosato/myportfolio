b6732842bcb3b87cf4ffa1471ef23ae8a936192f
Jennifer <st.jennyandy@gmail.com>
2023-11-19 05:05:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+
+import {BsLinkedin, BsTwitter, BsGithub} from 'react-icons/bs'
+// import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <header className='flex justify-evenly items-center shadow-md py-2'>
+     <div className='border-4 text-2xl'>Jio</div>
+     <ul className='flex gap-4 font-semibold'>
+      <li><a href='#'>Home</a></li>
+      <li><a href='#'>Projects</a></li>
+      <li><a href='#'>About Me</a></li>
+      
+     </ul>
+     <button className='shadow-md hover:bg-gray-300 border-none'>Contact Me</button>
+    </header>
+    {/* Intro section */}
+    <section>
+
+    </section>
+    {/* About Me Section */}
+    <section>
+
+    </section>
+    {/* Project Section */}
+    <section>
+
+    </section>
+    </>
+  ) 
+}
+
+export default App
