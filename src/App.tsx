3f51fa8d7bba27a978a687774cdff60daf8b4dbf
Jennifer <st.jennyandy@gmail.com>
2023-11-21 23:30:02 +0100

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 
 import {BsLinkedin, BsTwitter, BsGithub} from 'react-icons/bs'
+import bgvideo from '../src/assets/bgvid.mp4'
 // import './App.css'
 
 function App() {
  
   return (
     <>
+    <div>
+      <video autoPlay loop muted playsInline className='absolute right-0 bottom-0 border border-red-700' >
+        <source src='../src/assets/bgvid.mp4' type='video'/>
+      </video>
+    </div>
     <header className='flex justify-evenly items-center shadow-md py-2'>
      <div className='border-4 text-2xl'>Jio</div>
      <ul className='flex gap-4 font-semibold'>
